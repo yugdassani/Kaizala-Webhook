a466f560afa2eaabf649e35a17b088d79d1a3f1b
yugdassani <ydassani@gmail.com>
2019-06-14 13:19:02 +0530

    changes

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port %d', server.address().port);
+});
+
 app.get('/', (req, res) => {
 	// Display a test message
 	res.send('Hello World!')
