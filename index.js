f7a2161effd3522bb572cf567c2101668b19a97e
yugdassani <ydassani@gmail.com>
2019-06-14 14:37:56 +0530

    Redirection to flow

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 var express = require('express');
+var axios = require('axios');
 const fetch = require('node-fetch');
 
 var app = express();
@@ -27,6 +28,8 @@ fetch('/',{
 
 app.post('/', (req,res) => {
 	console.log(req.body)
+	axios.post("https://prod-87.westus.logic.azure.com:443/workflows/68e2aa63d92b489782f35b4972a7be4a/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Zdh5oGVMFBAzQAy6ly7I9DB4QTWqeXffpSALu4iQHeE",
+		req.body)
 })
 
 
